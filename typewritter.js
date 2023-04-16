a956ef40d2a3706cf1a41b83a8771257e931352b
hawa <121143106+hawa-mjh@users.noreply.github.com>
2023-04-15 23:33:22 -0400

    Create typewritter.js

---
+++ typewritter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay += 50);
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, sentence.length * 50 + 100);
